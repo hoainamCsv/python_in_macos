a96038b9eff93badee7ddf30f3170042a9d47e48
hoainamCsv <hoainam@vn-cubesystem.com>
2023-03-30 11:05:52 +0700

    Initial Commit

---
+++ test_python_swift/test_python_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_python_swiftApp.swift
+//  test_python_swift
+//
+//  Created by Cube System Vietnam on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct test_python_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
